400dbef2048df09371c89e989d9fbfd277a4220f
lisoflint01 <lisandro.antonioli@studio.unibo.it>
2023-12-15 16:47:18 +0100

    Start

--- src/GameStart.java
+++ src/GameStart.java
@@ -1,6 +1,5 @@
 public class GameStart {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
     }
 }
