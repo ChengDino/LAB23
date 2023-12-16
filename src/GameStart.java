930007038ad6019f10999bb70b4896a9542335bd
lisoflint01 <lisandro.antonioli@studio.unibo.it>
2023-12-16 15:23:59 +0100

    comments modified

--- src/GameStart.java
+++ src/GameStart.java
@@ -6,20 +6,20 @@ public class GameStart extends JFrame {
 
     public void Start() {
 
-        this.setSize(600, 600); // seleziono dimensione finestra
-        this.setLocationRelativeTo(null); // posizione della finestra sullo schermo
+        this.setSize(600, 600); // I select window size
+        this.setLocationRelativeTo(null); // position of the window on the screen
         this.setTitle("Snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
 
     }
 
-    // riscrivo metodo graphics
+    // I rewrite the graphics method
     @Override
     public void paint(Graphics g) {
 
         g.setColor(Color.GRAY);
-        g.fillRect(0, 0, 600, 600); // riempire la finestra (sfondo)
+        g.fillRect(0, 0, 600, 600); // fill the window (background)
         g.setColor(Color.BLACK);
         for (int i = 0; i <= 20; i++) {
             g.drawLine(0, i * 30, 600, i * 30);
