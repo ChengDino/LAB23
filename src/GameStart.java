413a00aff227ff6b7b970dd9be688bd6068a09e9
ChengDino <dino.cheng@studio.unibo.it>
2023-12-16 14:16:15 +0100

    finestra

--- src/GameStart.java
+++ src/GameStart.java
@@ -1,5 +1,21 @@
-public class GameStart {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javax.swing.JFrame;
+
+public class GameStart extends JFrame {
+
+    public void launch() {
+
+        this.setSize(600, 600); // seleziono dimensione finestra
+        this.setLocationRelativeTo(null); // posizione della finestra sullo schermo
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
     }
+
+    public static void main(String[] args) {
+        GameStart startgame = new GameStart();
+        startgame.launch();
+
+    }
+
 }
