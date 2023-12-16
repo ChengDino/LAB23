e8b343b0d8a1859101932db7043c5c9c9aa1a9e1
lisoflint01 <lisandro.antonioli@studio.unibo.it>
2023-12-16 14:58:36 +0100

    this is class GameObj

---
+++ src/GameObj.java
@@ -0,0 +1,77 @@
+import java.awt.Image;
+
+public class GameObj {
+    private Image img;
+    private int x;
+    private int y;
+    private int width = 30;
+    private int height = 30;
+    private GameStart frame;
+
+    // this is a costructor of class GameObj with parameters 
+    public GameObj(Image img, int x, int y, int width, int height, GameStart frame) {
+
+        this.img = img;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.frame = frame;
+        
+    }
+    
+    // this is a costructor of class GameObj without parameters 
+    public GameObj() {
+
+    }
+
+    public Image getImg() {
+        return img;
+    }
+
+    public void setImg(Image img) {
+        this.img = img;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public GameStart getFrame() {
+        return frame;
+    }
+
+    public void setFrame(GameStart frame) {
+        this.frame = frame;
+    }
+
+    
+}
