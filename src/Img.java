5153a2fcb38aea7445c25a8ec45f06414c02e0ac
lisoflint01 <lisandro.antonioli@studio.unibo.it>
2023-12-16 14:07:21 +0100

    file img with all image

---
+++ src/Img.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Img {
+    public static Image head_down = Toolkit.getDefaultToolkit()
+        .getImage("src\\img\\head_down.png");
+    public static Image head_up = Toolkit.getDefaultToolkit()
+        .getImage("src\\img\\head_up.png");
+    public static Image head_left = Toolkit.getDefaultToolkit()
+        .getImage("src\\img\\head_left.png");
+    public static Image head_right = Toolkit.getDefaultToolkit()
+        .getImage("src\\img\\head_right.png");
+    public static Image body = Toolkit.getDefaultToolkit()
+        .getImage("src\\img\\body.png");
+    public static Image food = Toolkit.getDefaultToolkit()
+        .getImage("src\\img\\food.png");
+}
