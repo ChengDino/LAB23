e3a73208cdaaf2740971a1a1985538be09fffa04
ChengDino <dino.cheng@studio.unibo.it>
2023-12-16 15:01:16 +0100

    background and grid

--- src/GameStart.java
+++ src/GameStart.java
@@ -1,4 +1,6 @@
 import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class GameStart extends JFrame {
 
@@ -12,11 +14,25 @@ public class GameStart extends JFrame {
 
     }
 
+    // riscrivo metodo graphics
+    @Override
+    public void paint(Graphics g) {
+
+        g.setColor(Color.GRAY);
+        g.fillRect(0, 0, 600, 600); // riempire la finestra (sfondo)
+        g.setColor(Color.BLACK);
+        for (int i = 0; i <= 20; i++) {
+            g.drawLine(0, i * 30, 600, i * 30);
+            g.drawLine(i * 30, 0, i * 30, 600);
+        }
+
+    }
+
     public static void main(String[] args) {
 
         GameStart startgame = new GameStart();
         startgame.Start();
-        
+
     }
 
 }
